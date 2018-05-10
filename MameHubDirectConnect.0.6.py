5bfcdb02baaa7a8792b1d03f6e27fd2baa4c0b30
Richard SABRAT <JustaLiriK@users.noreply.github.com>
2018-05-10 14:06:32 +0200

    Add files via upload

--- MameHubDirectConnect.0.6.py
+++ MameHubDirectConnect.0.6.py
@@ -1,9 +1,9 @@
 #-*- coding: utf-8 -*-
 #Version 0.6
 
-def Help() :
-        
-        ''' Script offrant un GUI minimal
+def Help()  :
+
+    ''' Script offrant un GUI minimal
     pour la connexion p2p à Csume64,émulateur utilisé par MAMEHub.
 
     *En mode SERVEUR (Héberger) :
@@ -22,18 +22,21 @@ def Help() :
     taper sf2 par exemple.
 
     TODO LIST:
-    -Ajouter un chat pour transmettre l'adresse IP. '''        
-import os , urllib.request ,io ,sys ,re
+    -Ajouter un chat pour transmettre l'adresse IP.
+    -Ajouter un lancement en mode solo? '''
+import os, urllib.request, sys, re
 from html.parser import HTMLParser
 from tkinter import *
 from tkinter.filedialog import *
+print(os.name)
+
 class Monipclean(HTMLParser):
     def handle_data(self, data):
-               print(data)
+        print(data)
 
 if len(sys.argv)>1:
        if sys.argv[1]=='-h' or '--help' :
-                print(Help.__doc__)        
+                print(Help.__doc__)
 global deco , parsed ,binpath ,exepath ,v3
 binpath=os.path.join('MAMEHubRepo','Binaries')
 try : os.chdir(binpath)
@@ -53,7 +56,7 @@ parser.feed(deco)
 
 fenetre=Tk()
 fenetre.title('MameHub Direct Connect -By JustaLiriK-')
-fenetre.geometry('490x120')
+fenetre.geometry('550x120')
 v1=StringVar()
 v2=StringVar()
 v3=StringVar()
@@ -65,7 +68,7 @@ def fichier():
         i=len(result)-2
         piz=result[i]
         e3.insert(0,piz)
-        
+
 
 Radiobutton(fenetre,text="Héberger" ,variable=r , value=1).grid(row=0, column=0)
 Radiobutton(fenetre, text="Rejoindre" ,variable=r , value=2).grid(row=0 , column=1)
@@ -84,7 +87,7 @@ b3.grid(row=4, column=0)
 l1=Label(fenetre, text="IP de l'hôte à joindre (mode client uniquement): ")
 l2=Label(fenetre, text="Entrez votre Pseudo ici : ")
 
-  
+
 l4=Label(fenetre, text=".zip")
 
 l1.grid(row=2,column=0)
@@ -97,14 +100,18 @@ def resetall():
     e1.delete(0, END)
     e2.delete(0, END)
     e3.delete(0,END)
-    
-def launch (): 
+
+def launch ():
     mode=r.get()
     player=e2.get()
-         
+
     rom=e3.get()
     host=e1.get()
-    exepath=os.path.join('dist','csume64.exe')
+
+    if os.name=="nt":
+        exepath=os.path.join('dist','csume64.exe')
+    else:
+        exepath=os.path.join('dist','csume64')
     try : exepath
     except FileNotFoundError :
         print('/!\ Chemin incorrect, fermer l\'applicaton et vérifier que le l\'exécutable soit présent')
@@ -113,15 +120,17 @@ def launch ():
            # execute="dist\csume64.exe " + rom +" -server -port 6805  -username "+ player
            execute=exepath +" "+ rom +" -server -port 6805  -username "+ player
     else :
-           
+
           # execute="dist\csume64.exe " + rom +" -client -port 6805 -hostname " + host + " -username " + player
           execute=exepath +" "+ rom +" -client -port 6805 -hostname " + host + " -username " + player
     print("c'est parti!")
     print( execute)
     os.system(execute)
-    
+
 b1=Button(fenetre, text="R.A.Z.", command=resetall, cursor='exchange')
 b2=Button(fenetre, text="Lancer" , command=launch, cursor='shuttle')
 b1.grid(row=5, column=1)
 b2.grid(row=5 , column=2)
 mainloop()
+
+
